d2125f7b9ba71b3789642a942148925357f6f240
radek@skrabal.me <radek@skrabal.me@5df13726-ab30-94f0-ca3f-5d815ade5363>
2012-11-10 12:03:31 +0000

    GC 160: Add errorception.com account for I:ZI Miner

--- tools/IZI Miner/web/js/ext/errorception.js
+++ tools/IZI Miner/web/js/ext/errorception.js
@@ -1,4 +1,4 @@
-var _errs=["506ac563d3f654c85c000d91"];(function(a,b){a.onerror=function(){_errs.push(arguments)};
+var _errs=["509244bf649f413804001da7"];(function(a,b){a.onerror=function(){_errs.push(arguments)};
     var d=function(){var a=b.createElement("script"),c=b.getElementsByTagName("script")[0];
         a.src="//d15qhc0lu1ghnk.cloudfront.net/beacon.js";a.async=!0;
         c.parentNode.insertBefore(a,c)};a.addEventListener?a.addEventListener("load",d,!1):
