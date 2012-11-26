e1030a9cf8d260b2ab9ef89037f436ba50492e08
radek@skrabal.me <radek@skrabal.me@5df13726-ab30-94f0-ca3f-5d815ade5363>
2012-11-26 18:44:41 +0000

    GC 157: UI issues for I:ZI Miner

--- trunk/tools/IZI Miner/web/js/lib/UIColorizer.js
+++ trunk/tools/IZI Miner/web/js/lib/UIColorizer.js
@@ -1,13 +1,20 @@
 var UIColorizer = new Class({
-	
+
+    tween: function(element, property, value, duration) {
+        new Fx.Tween(element, {duration: duration}).start(property, value);
+    },
+
+    // TODO remove - obsolete
 	dragEnter: function (element) {
         element.tween('background-color', '#98B5C1');
 	},
-	
+
+    // TODO remove - obsolete
 	dragLeave: function (element) {
         element.tween('background-color', '#FFF');
 	},
-	
+
+    // TODO remove - obsolete
 	dragDrop: function (element) {
         element.highlight('#7389AE');
 	}
