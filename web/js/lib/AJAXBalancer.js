293293f907f1ed993a30e0bf24d74c4e04788051
Stanislav Vojíř <stanislav.vojir@gmail.com>
2015-02-10 00:34:43 +0100

    úprava ajax balanceru (odstraňování hotových požadavků, omezení počtu pokusů na poslání požadavku)
    fixed #34

--- web/js/lib/AJAXBalancer.js
+++ web/js/lib/AJAXBalancer.js
@@ -4,20 +4,38 @@ var AJAXBalancer = new Class({
 	limit: 5,
 	running: 0,
 	requests: [],
+	limitRuns: 2,
 	
 	initialize: function () {},
 	
 	addRequest: function (options, data, ruleId) {
+		var date=new Date();
+		var simpleUid=date.getTime()+(1000+Math.floor((Math.random() * 1000)));
+
 		options.onComplete = function () {
-			this.running--;
-			this.send();
+			this.handleCompletedRequest(simpleUid);
 		}.bind(this);
-		
-		var req = [new Request.JSON(options), data, ruleId];
-		
+
+		var req = [new Request.JSON(options), data, ruleId, simpleUid, 0];
 		this.requests.push(req);
 	},
-	
+
+	handleCompletedRequest : function(simpleUid){
+		Array.each(this.requests, function (req, key) {
+			if (req[3]==simpleUid){
+				if (!req[0].isRunning()){
+					if (req[0].isSuccess() || req[3]>this.limitRuns){
+						//pokud je požadavek dokončen, nebo byl překročen počet pokusů, odstraníme ho z pole
+						this.requests.splice(key,1);
+					}
+				}
+			}
+		}.bind(this));
+
+		this.running--;
+		this.send();
+	},
+
 	run: function () {
 		if (this.requests.length) {
 			this.inProgress = true;
@@ -29,19 +47,12 @@ var AJAXBalancer = new Class({
 		Array.each(this.requests, function (req, key) {
 			if (this.running < this.limit) {
 				if (!req[0].isRunning() && !req[0].isSuccess()) {
+					req[4]++;
 					this.running++;
 					req[0].post({'data': req[1]});
 				}
 			}
 		}.bind(this));
-	},
-	
-	handleReqSuccess: function () {
-		
-	},
-	
-	handleReqError: function () {
-		
 	},
 	
 	stopRequest: function (ruleId) {
