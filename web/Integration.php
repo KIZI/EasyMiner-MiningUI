e7bd02f250a7067d0d3c006b9fabe2c93295f845
Stanislav Vojíř <stanislav.vojir@gmail.com>
2015-03-18 22:57:49 +0100

    integrace MiningUI s EasyMinerCenter

---
+++ web/Integration.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace EasyMiner\MiningUI;
+
+class Integration {
+  /**
+   * @var $javascriptFiles - this array contains list of javascript files for inclusion into page
+   */
+  public static $javascriptFiles = [
+    //"js/ext/errorception.js",
+    'js/ext/cryptojs-3.0.2-md5.js',
+    'js/ext/mootools-core-1.4.5.js',
+    'js/ext/mootools-more-1.4.0.1.js',
+    'js/lib/Mutators.js',
+    'js/ext/mooml-1.2.4.js',
+    'js/Bootstrap.js',
+    'js/lib/Properties.js',
+    'js/lib/Config.js',
+    'js/lib/AJAXBalancer.js',
+    'js/lib/APField.js',
+    'js/lib/ARBuilder.js',
+    'js/lib/ARManager.js',
+    'js/lib/AssociationRule.js',
+    'js/lib/AssociationRuleValidator.js',
+    'js/lib/Attribute.js',
+    'js/lib/AttributeSorter.js',
+    'js/lib/BBACoefficient.js',
+    'js/lib/BrowserDetector.js',
+    'js/lib/Cedent.js',
+    'js/lib/CoefficientAddSlider.js',
+    'js/lib/CoefficientEditSlider.js',
+    'js/lib/ColorHelper.js',
+    'js/lib/Connective.js',
+    'js/lib/CurrentUser.js',
+    'js/lib/DataDescription.js',
+    'js/lib/DateHelper.js',
+    'js/lib/DataParser.js',
+    'js/lib/DragDropHelper.js',
+    'js/lib/ElementSizeMeter.js',
+    'js/lib/ElementFilter.js',
+    'js/lib/ETreeManager.js',
+    'js/lib/ETreeValidator.js',
+    'js/lib/FeatureList.js',
+    'js/lib/Field.js',
+    'js/lib/FieldAR.js',
+    'js/lib/FieldFR.js',
+    'js/lib/FieldGroup.js',
+    'js/lib/FieldGroupConfig.js',
+    'js/lib/FoundRule.js',
+    'js/lib/FRManager.js',
+    'js/lib/i18n.js',
+    'js/lib/IMValidator.js',
+    'js/lib/InterestMeasure.js',
+    'js/lib/InterestMeasureSlider.js',
+    'js/lib/InterestMeasureAR.js',
+    'js/lib/MarkedRule.js',
+    'js/lib/MarkedTask.js',
+    'js/lib/MiningManager.js',
+    'js/lib/MRManager.js',
+    'js/lib/NativeTypeExtender.js',
+    'js/lib/NumberNormalizer.js',
+    'js/lib/Report.js',
+    'js/lib/ReportManager.js',
+    'js/lib/RulePattern.js',
+    'js/lib/RulesParser.js',
+    'js/lib/Settings.js',
+    'js/lib/Storage.js',
+    'js/lib/StringHelper.js',
+    'js/lib/Task.js',
+    'js/lib/TaskManager.js',
+    'js/lib/UIColorizer.js',
+    'js/lib/UIListener.js',
+    'js/lib/UIPainter.js',
+    'js/lib/UIScroller.js',
+    'js/lib/UIStructureListener.js',
+    'js/lib/UIStructurePainter.js',
+    'js/lib/UIStructureTemplater.js',
+    'js/lib/UITemplateRegistrator.js',
+    'js/lib/URLHelper.js'
+  ];
+
+  /**
+   * @var $cssFiles - this array contains list of CSS files for inclusion into page
+   */
+  public static $cssFiles = [
+    'css/index.css'
+  ];
+
+}
